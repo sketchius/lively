8e1e5fe5a7b340f8d0cbcc4d160d41179079e26b
Bryce Huhtala <sketchius@gmail.com>
2023-11-14 12:15:20 -0600

    Remove memory directory

--- functions/memory/observationManager.js
+++
@@ -1,8 +0,0 @@
-import { createObservationForUser } from '../database/memoryData.js';
-import { createEmbedding, upsertEmbeddingsToPinecone } from '../external_apis/index.js'
-
-const saveDataAsVector = async (userId, data, referenceId, conversationId) => {
-  const embedding = await createEmbedding(data);
-  upsertEmbeddingsToPinecone(embedding, referenceId);
-  createObservationForUser(userId, data, referenceId, conversationId);
-}
