02884fa346586e1f1e2801b223754bede457a18a
Bryce Huhtala <sketchius@gmail.com>
2023-11-15 09:11:44 -0600

    Clean up unused code

--- functions/chat/messageHandler.js
+++ functions/chat/messageHandler.js
@@ -20,85 +20,10 @@ import {
   summarizeConversation,
 } from "../processing/index.js";
 
-const functions = [
-  {
-    name: "query_memory",
-    description: "SEARCH memory for CONTEXT MISSING in conversation",
-    parameters: {
-      type: "object",
-      properties: {
-        people: {
-          type: "array",
-          description:
-            "a list of SPECIFIC people, pets, characters, or creatues to search for",
-          items: {
-            type: "string",
-            description: "<specifier/relationship> <name>",
-          },
-        },
-        events: {
-          type: "array",
-          description: "a list of events, stories, experiences to search for",
-          items: {
-            type: "string",
-            description: "<name> <time>",
-          },
-        },
-        places: {
-          type: "array",
-          description: "a list of specific places",
-          items: {
-            type: "string",
-            description: "<specifier> <name>",
-          },
-        },
-        things: {
-          type: "array",
-          description: "a list of non-living personal things",
-          items: {
-            type: "string",
-            description: "<specifier> <name>",
-          },
-        },
-      },
-    },
-  },
-];
 
 const handleMessage = async (messageBody) => {
   await addMessageToConversation("user", messageBody);
-  // await new Promise((resolve) => setTimeout(resolve, 3000)); // Sleep for 3000 milliseconds
-  // let currentConversationId = await getCurrentConversationId("test");
 
-  // await summarizeConversation("test", currentConversationId);
-  // await calcAverageWordCount("test", currentConversationId);
-
-  // const avgWords =
-  //   (await getConversationProperty(
-  //     "test",
-  //     currentConversationId,
-  //     "averageWordCount"
-  //   )) || 20;
-  // const writingStyle =
-  //   (await getConversationProperty("test", currentConversationId, "summary")) ||
-  //   "Normal.";
-  // console.log(`avgWords: ${avgWords}`);
-  // console.log(`
-  // *******************************
-  // writingStyle: ${writingStyle}`);
-
-  //   const sysMessage =
-  //   `# IDENTITY
-  // You're a digital journal. You're job is to listen to the user talk about their day. Try to not be too direct or forward with questioning. LIMIT your response length to ${Math.round(
-  //     avgWords * 1
-  //   )}-${Math.round(avgWords * 2)} words. Try to keep the conversation going.
-  // # RESPONSE OUTPUT REQUIREMENTS
-  // Your response MUST FOLLOW this writing style: ${writingStyle}
-  // `
-
-  // const sysMessage = `You are memory_bot. SCAN the CONVERSATION for ALL PEOPLE, PLACES, THINGS, or EVENTS that require additional context. If nothing needs additional context, reply N/A.
-  // Use query_memory function to search for additional context. ADD SPECIFIERS TO PREVENT AMBIGUITY (i.e. *my friend* Bob, party *next week*, *big* TV)
-  // `;
   if (messageBody.includes("!clear")) {
     await deleteAllVectors();
   } else {
