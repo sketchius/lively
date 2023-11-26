df85c7c9acd3067eb1671de9adb1d4d0410092cf
Bryce Huhtala <sketchius@gmail.com>
2023-11-25 21:31:21 -0600

    Add relative date language processor

--- functions/agents/dateAgent.js
+++ functions/agents/dateAgent.js
@@ -1,16 +1,92 @@
 import { getOpenAIChatResponse } from "../external_apis/index.js";
 
+const processRelativeDates = async (text, originalDate) => {
+    const systemMessage = `# Task: Recalibrate past dates or time references to align with the current date.
+    # CURRENT DATE: ${new Date()}
+    # DATE WRITTEN: ${originalDate}`;
+
+  const promptMessage = `# COMMAND Use relative language to convert this statement from present to past. BE SPECIFIC about how long ago. For GENERAL statements, keep present tense.
+  # STATEMENT ${text}`;
+
+  const messages = [
+    {
+      role: "system",
+      content: systemMessage,
+    },
+    {
+      role: "user",
+      content: promptMessage,
+    },
+  ];
+
+  const response = await getOpenAIChatResponse(messages, {});
+
+  return await response.content;
+};
+
+const detectTemporalReferences = async (text) => {
+    const systemMessage = `# Task: Analyze the text to determine if there are any temporal references.
+    # OUTPUT: ONLY Yes or No.`;
+
+  const promptMessage = `${text}`;
+
+  const messages = [
+    {
+      role: "system",
+      content: systemMessage,
+    },
+    {
+      role: "user",
+      content: promptMessage,
+    },
+  ];
+
+  const response = await getOpenAIChatResponse(messages, {});
+
+  if (!response.content.toLowerCase().contains('yes')) {
+    return false;
+  } else {
+    return true;
+  }
+
+  return await response.content;
+};
+
+
+
 const convertDatesFromRelativeToAbsolute = async (text) => {
-  const systemMessage = `# ROLE: You are a Relative -> Absolute Date Converter.
-  # METHODOLOGY: You SEARCH text for RELATIVE DATES and REPLACE matches with ABSOLUTE DATES. ONLY change the DATES. Your response should ONLY contain the MODIFIED TEXT.
-  # CURRENT DATE: 11/18/23
-  # EXAMPLE INPUT/OUTPUT
-  INPUT: "Tommorow works for me!", OUTPUT: "11/19/23 works for me!",
-  INPUT: "Yesterday I did.", OUTPUT: "On 11/17/23 I did";
-  INPUT: "Next week I'll be back", OUTPUT: "The week of  11/20/23 I'll be back.";
-  INPUT: "It'll be a couple months.", OUTPUT: "It'll be around January.";
-  INPUT: "I haven't seen him in a couple years.", OUTPUT: "I haven't seen him since around 2021.";
-  INPUT: "Joe was there last Christmas.", OUTPUT: "Joe was there Christmas 2022."`;
+    const today = new Date();
+
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const nextWeek = new Date(today);
+    nextWeek.setDate(nextWeek.getDate() + 7);
+
+    const aCoupleMonths = new Date(today);
+    aCoupleMonths.setMonth(aCoupleMonths.getMonth() + 2);
+
+    const aCoupleYearsAgo = new Date(today);
+    aCoupleYearsAgo.setFullYear(aCoupleYearsAgo.getFullYear() - 2);
+
+    const lastChristmas = new Date(`12/25/${today.getFullYear() - 1}`);
+    
+    const systemMessage = `# ROLE: You are a Relative -> Absolute Date Converter.
+      # METHODOLOGY: You SEARCH text for RELATIVE DATES and REPLACE matches with ABSOLUTE DATES. ONLY change the DATES. Your response should ONLY contain the MODIFIED TEXT. Do NOT change AGES or DURATIONS.
+      # CURRENT DATE: ${today.toLocaleDateString()}
+      # EXAMPLE INPUT/OUTPUT
+      INPUT: "Tomorrow works for me!", OUTPUT: "${tomorrow.toLocaleDateString()} works for me!",
+      INPUT: "Yesterday I did.", OUTPUT: "On ${yesterday.toLocaleDateString()} I did.",
+      INPUT: "Next week I'll be back", OUTPUT: "The week of  ${nextWeek.toLocaleDateString()} I'll be back.",
+      INPUT: "It'll be a couple months.", OUTPUT: "It'll be around ${aCoupleMonths.toLocaleString('default', { month: 'long' })}.",
+      INPUT: "I haven't seen him in a couple years.", OUTPUT: "I haven't seen him since around ${aCoupleYearsAgo.getFullYear()}.",
+      INPUT: "Joe was there last Christmas.", OUTPUT: "Joe was there Christmas ${lastChristmas.getFullYear()}.",
+      INPUT: "3 years old", OUTPUT: "3 years old",
+      INPUT: "currently", OUTPUT: "As of ${today.toLocaleDateString()}"`;
+    
 
   const promptMessage = `# COMMAND: Output text with any RELATIVE dates replaced with ABSOLUTE dates.
   # TEXT:
@@ -28,10 +104,8 @@ ${text}`;
   ];
 
   const response = await getOpenAIChatResponse(messages, {});
-  console.log("RESPONSE:");
-  console.log(response.content);
 
   return await response.content;
 };
 
-export { convertDatesFromRelativeToAbsolute };
+export { convertDatesFromRelativeToAbsolute, detectTemporalReferences };
