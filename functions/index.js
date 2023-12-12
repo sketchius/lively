548a2c580039e6106d4c45b98302afbe65cddcd5
Bryce Huhtala <sketchius@gmail.com>
2023-12-12 08:50:19 -0600

    Use test user for production

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,16 @@ import * as functions from "firebase-functions";
 import { chatApp } from "./chat/index.js";
 
 import "dotenv/config.js";
+import { dataApp } from "./data/index.js";
 
 export const chat = functions
   .runWith({
     timeoutSeconds: 300,
   })
   .https.onRequest(chatApp);
+
+export const data = functions
+  .runWith({
+    timeoutSeconds: 300,
+  })
+  .https.onRequest(dataApp);
