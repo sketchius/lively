48fd528db26c47b5cc17611e376ea9c8533877de
Bryce Huhtala <sketchius@gmail.com>
2024-01-07 17:30:46 -0600

    Create sidebar

---
+++ public/src/components/SidebarMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/tasks">Tasks</router-link></li>
+      <li><router-link to="/goals">Goals</router-link></li>
+      <li><router-link to="/habits">Habits</router-link></li>
+      <li><router-link to="/routines">Routines</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarMenu",
+};
+</script>
+
+<style scoped>
+.sidebar ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.sidebar ul li {
+  margin: 10px 0;
+}
+</style>
