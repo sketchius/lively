113f43d98ec4485c010bc8cfe8126f9bfb4d9236
Bryce Huhtala <sketchius@gmail.com>
2024-02-01 13:26:41 -0600

    Update backend routes to reflect new schema

--- functions/data/data.js
+++ functions/data/data.js
@@ -59,99 +59,10 @@ export const goalData = {
 
   async removeObjectiveFromGoals(userId, objectiveId) {
     await firestore.removeObjectiveFromGoals(userId, objectiveId);
-  }
-};
-
-export const objectiveData = {
-  async createObjective(userId, objectiveData) {
-    const uid = objectiveData.id ? objectiveData.id : createUID();
-    const path = `users/${userId}/objectives/${uid}`;
-    await firestore.create(path, objectiveData);
-    return uid;
-  },
-
-  async getObjective(userId, objectiveId) {
-    const path = `users/${userId}/objectives/${objectiveId}`;
-    return await firestore.read(path);
-  },
-
-  async listObjectives(userId) {
-    const path = `users/${userId}/objectives`;
-    return await firestore.list(path);
-  },
-
-  async updateObjective(userId, objectiveId, objectiveData) {
-    const path = `users/${userId}/objectives/${objectiveId}`;
-    await firestore.update(path, objectiveData);
-  },
-
-  async deleteObjective(userId, objectiveId) {
-    const path = `users/${userId}/objectives/${objectiveId}`;
-    await firestore.delete(path);
-  },
-
-  async createPlan(userId, objectiveData) {
-    const uid = createUID();
-    const path = `users/${userId}/objectives/${objectiveId}/plans/${uid}`;
-    await firestore.create(path, objectiveData);
-    return uid;
-  },
-
-  async getPlan(userId, objectiveId, planId) {
-    const path = `users/${userId}/objectives/${objectiveId}/plans/${planId}`;
-    return await firestore.read(path);
-  },
-
-  async listPlans(userId, objectiveId) {
-    const path = `users/${userId}/objectives/${objectiveId}/plans`;
-    return await firestore.list(path);
-  },
-
-  async updatePlan(userId, objectiveId, planId, planData) {
-    const path = `users/${userId}/objectives/${objectiveId}/plans/${planId}`;
-    await firestore.update(path, planData);
-  },
-
-  async deletePlan(userId, objectiveId, planId) {
-    const path = `users/${userId}/goals/${goalId}/plans/${planId}`;
-    await firestore.delete(path);
   },
   
-  async removeTaskFromObjectives(userId, taskId) {
-    await firestore.removeTaskFromObjectives(userId, taskId);
-  }
-};
-
-export const taskData = {
-  async createTask(userId, taskData) {
-    const uid = taskData.id ? taskData.id : createUID();
-    const path = `users/${userId}/tasks/${uid}`;
-    await firestore.create(path, taskData);
-    return uid;
-  },
-
-  async getTask(userId, taskId) {
-    const path = `users/${userId}/tasks/${taskId}`;
-    return await firestore.read(path);
-  },
-
-  async listTasks(userId) {
-    const path = `users/${userId}/tasks`;
-    return await firestore.list(path);
-  },
-
-  async updateTask(userId, taskId, taskData) {
-    const path = `users/${userId}/tasks/${taskId}`;
-    await firestore.update(path, taskData);
-  },
-
-  async updateTaskStatus(userId, taskId, newStatus) {
-    const path = `users/${userId}/tasks/${taskId}`;
+  async updateGoalStatus(userId, goalId, newStatus) {
+    const path = `users/${userId}/goals/${goalId}`;
     await firestore.updateField(path, "complete", newStatus);
-  },
-
-  async deleteObjective(userId, taskId) {
-    const path = `users/${userId}/tasks/${taskId}`;
-    await firestore.delete(path);
   },
 };
--- functions/data/index.js
+++ functions/data/index.js
@@ -143,8 +143,6 @@ dataApp.put("/goals/:goalId", async (req, res) => {
   }
 });
 
-
-
 dataApp.delete("/goals/:goalId", async (req, res) => {
   try {
     const userId = getUserId();
@@ -155,160 +153,15 @@ dataApp.delete("/goals/:goalId", async (req, res) => {
   }
 });
 
-// Objective routes
-dataApp.post("/objectives", async (req, res) => {
+dataApp.put("/goals/:goalId/completion", async (req, res) => {
   try {
     const userId = getUserId();
-    const data = req.body;
-
-    for (let j = 0; j < data.tasks.length; j++) {
-      const task = data.tasks[j];
-      task.id = await taskData.createTask(userId, task.data);
-      delete task.data;
-    }
-
-    const objectiveId = await objectiveData.createObjective(userId, data);
-    res.json({ objectiveId });
+    await goalData.updateGoalStatus(userId, req.params.goalId, req.body);
+    res.send("Goal updated successfully");
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
-dataApp.get("/objectives/:objectiveId", async (req, res) => {
-  try {
-    const userId = getUserId();
-    const objective = await objectiveData.getObjective(
-      userId,
-      req.params.objectiveId
-    );
-    res.json(objective);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.get("/objectives", async (req, res) => {
-  try {
-    const userId = getUserId();
-    const objectives = await objectiveData.listObjectives(userId);
-
-    for (let i = 0; i < objectives.length; i++) {
-      const objective = objectives[i];
-
-      if (objective.tasks) {
-        for (let j = 0; j < objective.tasks.length; j++) {
-          const task = objective.tasks[j];
-
-          task.data = await taskData.getTask(userId, task.id);
-        }
-      }
-    }
-
-    res.json(objectives);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.put("/objectives/:objectiveId", async (req, res) => {
-  try {
-    const userId = getUserId();
-    const data = req.body;
-
-    for (let i = 0; i < data.tasks.length; i++) {
-      const task = data.tasks[i];
-      if (task.data.modified) {
-        if (task.data.new) {
-          task.data.new = false;
-          const taskId = await taskData.createTask(userId, task.data);
-          task.id = taskId;
-        } else {
-          await taskData.updateTask(userId, task.id, task.data);
-        }
-      }
-      delete task.data;
-    }
-
-    await objectiveData.updateObjective(userId, req.params.objectiveId, data);
-    res.status(200).send("Objective updated.");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.delete("/objectives/:objectiveId", async (req, res) => {
-  try {
-    const userId = getUserId();
-    await objectiveData.deleteObjective(userId, req.params.objectiveId);
-    await goalData.removeObjectiveFromGoals(userId, req.params.objectiveId);
-    res.send("Objective deleted successfully");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error.message);
-  }
-});
-
-// Task routes
-dataApp.post("/tasks", async (req, res) => {
-  try {
-    const userId = getUserId();
-    const taskId = await taskData.createTask(userId, req.body);
-    res.json({ taskId });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.get("/tasks/:taskId", async (req, res) => {
-  try {
-    const userId = getUserId();
-    const task = await taskData.getTask(userId, req.params.taskId);
-    res.json(task);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.get("/tasks", async (req, res) => {
-  try {
-    const userId = getUserId();
-    const tasks = await taskData.listTasks(userId);
-    res.json(tasks);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.put("/tasks/:taskId", async (req, res) => {
-  try {
-    const userId = getUserId();
-    await taskData.updateTask(userId, req.params.taskId, req.body);
-    res.send("Task updated successfully");
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.put("/tasks/:taskId/completion", async (req, res) => {
-  try {
-    const userId = getUserId();
-    await taskData.updateTaskStatus(userId, req.params.taskId, req.body);
-    res.send("Task updated successfully");
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-dataApp.delete("/tasks/:taskId", async (req, res) => {
-  try {
-    const userId = getUserId();
-    await taskData.deleteTask(userId, req.params.taskId);
-    res.send("Task deleted successfully");
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
 
 export { dataApp };
