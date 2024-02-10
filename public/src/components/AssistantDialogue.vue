9b208ab37b1cc0ca61151ceb47489f9fb02551e6
Bryce Huhtala <sketchius@gmail.com>
2024-02-10 15:26:30 -0600

    Develop assistant dialogue styling

--- public/src/components/AssistantDialogue.vue
+++ public/src/components/AssistantDialogue.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="dialogue">
-    <div class="icon-container"><div class="icon"></div></div>
+  <div class="dialogue" :class="{ 'vertical': props.layout == 'vertical'}">
+    <div class="icon-container" @click="startAnimation">
+      <div class="icon">
+        <div class="gradient" :class="{ 'animate-once': animate }"></div>
+      </div>
+    </div>
     <div class="content" :class="{ gap: props.subtext }">
       <div class="message">
         {{ props.message }}
@@ -13,17 +17,27 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
+
+const animate = ref(false);
 
 const props = defineProps({
-    message: String,
-    subtext: String
-})
+  message: String,
+  subtext: String,
+  layout: String
+});
 
+const startAnimation = () => {
+  if (!animate.value) {
+    animate.value = true;
+    setTimeout(() => {
+      animate.value = false;
+    }, 3000);
+  }
+};
 </script>
 
 <style scoped>
-
 .dialogue {
   display: flex;
   align-items: center;
@@ -38,13 +52,17 @@ const props = defineProps({
   box-sizing: border-box;
 }
 
+.vertical {
+  flex-direction: column;
+}
+
 .content {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .content.gap {
-    grid-gap: var(--size1)
+  grid-gap: var(--size1);
 }
 
 .message {
@@ -65,12 +83,70 @@ const props = defineProps({
   border-right: 1px dashed var(--ink);
 }
 
+.vertical .icon-container {
+  margin-bottom: 10px;
+  padding: 5px 0;
+  border-right: none;
+  border-bottom: 1px dashed var(--ink);
+}
+
 .icon-container .icon {
   width: 20px;
   height: 20px;
-  margin-right: 9px;
-  background: linear-gradient( 45deg, var(--red) 0%, var(--red) 20%, var(--yellow) 40%, var(--green) 60%, var(--blue) 80%, var(--blue) 90%);
-  border: 3px solid var(--ink);
+  margin-right: 14px;
   transform: rotate(45deg);
+  border: 5px solid var(--ink);
 }
+
+
+.vertical .icon-container .icon {
+  margin-left: 7px;
+  margin-right: 7px;
+  margin-bottom: 3px;
+}
+
+.gradient {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    45deg,
+    var(--red) 0%,
+    var(--yellow) 12.5%,
+    var(--green) 25%,
+    var(--blue) 37.5%,
+    var(--red) 50%,
+    var(--yellow) 62.5%,
+    var(--green) 75%,
+    var(--blue) 87.5%,
+    var(--red) 100%
+  );
+  background-position: 110% 32.5%;
+  background-size: 230% 200%;
+}
+
+.animate-once {
+  background-size: 230% 200%;
+  background-position: -50% 0%;
+  animation: gradient 3s ease-in-out reverse;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: -245% 32.5%;
+    background-size: 230% 200%;
+  }
+
+  100% {
+    background-position: 375% 130%;
+  }
+}
+/* @keyframes gradient {
+  0% {
+    background-position: 400% 130%;
+    transform: rotate(45deg);
+  }
+  100% {
+    transform: rotate(405deg);
+  }
+} */
 </style>
