4bd37b5d33fcafaf1e58827d100157df5d4e0844
Bryce Huhtala <sketchius@gmail.com>
2024-02-17 13:42:54 -0600

    Implement text-to-item back end

--- functions/assistant/contoller.js
+++ functions/assistant/contoller.js
@@ -1,36 +1,21 @@
 import { getOpenAIChatResponse } from "../external_apis/index.js";
 
 export const assistantController = {
-  async getItemFromDescription(description) {
+  async getItemFromDescription(description, type) {
     const messages = [
+
       {
         role: "system",
         content: `# INPUT A user's description of a goal.
-                # OUTPUT SCHEMA:
-                {
-                    "title" (string): "a clear and concise name for the goal. should start with a verb. (max 20 characters)",
-                    "category" (enum): ["work","household","errand","life management","fun"],
-                    "timeframe" (number): "a target amount of days to finish the goal",
-                    "internalNotes" (string): "additional information about the user's goal for future reference. only include data not covered by the other fields"
-                }
-                # FUNCTION Convert the user's description into suggestions for the fields in the output schema. If no timeframe is included in the input, provide a recommendation based on the nature of the goal. Attempt to extract the ultimate goal, but make note of any obstacles that the user mentions.`,
-      },
-      {
-        role: "user",
-        content: description,
-      },
-    ];
-
-    return await getOpenAIChatResponse(messages, true);
-  },
-
-  async validateDescription(description) {
-    const messages = [
-      {
-        role: "system",
-        content: `# INPUT A user's description of a goal.
-        # OUTPUT enum ["valid","invalid"] // always output one of the enum values
-        # FUNCTION Determine whether a goal can be extracted from the INPUT
+        # OUTPUT SCHEMA:
+        {
+           "valid" (boolean): "Whether a goal can be extracted from the INPUT. If *false*, exclude all other fields."
+            "title" (string): "a clear and concise name for the goal. should start with a verb. (max 20 characters)",
+            "category" (enum): ["work","household","errand","life management","fun"],
+            "timeframe" (number): "a target amount of days to finish the goal",
+            ${type == "Task" ? `"duration" (number): "an estimated time in minutes the task with take"`:''}
+        }
+        # FUNCTION Convert the user's description into suggestions for the fields in the output schema. If no timeframe is included in the input, provide a recommendation based on the nature of the goal.
         `,
       },
       {
@@ -39,6 +24,6 @@ export const assistantController = {
       },
     ];
 
-    return await getOpenAIChatResponse(messages, false);
+    return await getOpenAIChatResponse(messages, true);
   },
 };
--- functions/assistant/index.js
+++ functions/assistant/index.js
@@ -6,29 +6,10 @@ const assistantApp = express();
 assistantApp.use(express.json());
 assistantApp.use(cors({ origin: true }));
 
-function getUserId() {
-  return "test";
-}
-
 assistantApp.post("/text-to-item/description", async (req, res) => {
-  const result = await assistantController.getItemFromDescription(req.body);
+  const result = await assistantController.getItemFromDescription(req.body.data, req.body.type);
 
   res.json(result);
-  // if (result.status == "success") {
-  //   res.json(result.data);
-  // } else {
-  //   if (result.status == "internal-error") {
-  //     res.status(500).send(result.error);
-  //   } else {
-  //     res.status(400).send(result.error);
-  //   }
-  // }
-});
-
-assistantApp.post("/text-to-item/validate", async (req, res) => {
-  const result = await assistantController.validateDescription(req.body);
-
-  res.status(200).send(result == 'valid');
 });
 
 export { assistantApp };
