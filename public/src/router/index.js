0606b5988b85992214d4154d138c0cbac9cdaf12
Bryce Huhtala <sketchius@gmail.com>
2024-03-05 10:12:44 -0600

    Tidy store and router

--- public/src/router/index.js
+++ public/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/HomeView.vue";
-// import Tasks from "../task-list/view/TaskList.vue";
+import Tasks from "../task-list/view/TaskList.vue";
 import Notes from "../note-list/view/NoteList.vue";
 import Goals from "../goal-list/view/GoalList.vue";
 import ItemEditor from "../item-editor/view/ItemEditor.vue";
@@ -18,6 +18,11 @@ const routes = [
     component: Notes,
   },
   {
+    path: "/tasks",
+    name: "Tasks",
+    component: Tasks,
+  },
+  {
     path: "/goals",
     name: "Goals",
     component: Goals,
@@ -26,49 +31,6 @@ const routes = [
     path: "/item-editor",
     component: ItemEditor,
     name: "item-editor-undefined",
-    meta: { editorStep: -1 },
-  },
-  {
-    path: "/item-editor/type",
-    component: ItemEditor,
-    name: "item-editor-1",
-    meta: { editorStep: 1 },
-  },
-  {
-    path: "/item-editor/describe",
-    component: ItemEditor,
-    name: "item-editor-2",
-    meta: { editorStep: 2 },
-  },
-  {
-    path: "/item-editor/title",
-    component: ItemEditor,
-    name: "item-editor-3",
-    meta: { editorStep: 3 },
-  },
-  {
-    path: "/item-editor/priority",
-    component: ItemEditor,
-    name: "item-editor-4",
-    meta: { editorStep: 4 },
-  },
-  {
-    path: "/item-editor/timeframe",
-    component: ItemEditor,
-    name: "item-editor-5",
-    meta: { editorStep: 5 },
-  },
-  {
-    path: "/item-editor/summary",
-    component: ItemEditor,
-    name: "item-editor-6",
-    meta: { editorStep: 6 },
-  },
-  {
-    path: "/item-editor/steps",
-    component: ItemEditor,
-    name: "item-editor-7",
-    meta: { editorStep: 7 },
   },
 ];
 
--- public/src/store/index.js
+++ public/src/store/index.js
@@ -1,33 +1,26 @@
 import { createStore } from "vuex";
 import dataService from "@/services/dataService";
-import { createUID } from "@/util/uuid";
 
 export default createStore({
   state() {
     return {
-      currentEditorGoal: {},
-      currentEditorObjective: {},
-      currentEditorTask: {},
-      commandStack: [],
-      returnValue: { type: "null", data: null },
       goals: [],
-      currentRightPanelComponent: "CreateGoal",
-      editingGoal: null,
+      tasks: [],
+      notes: [],
       goalListNeedsRefresh: false,
-      objectiveListNeedsRefresh: false,
       taskListNeedsRefresh: false,
+      noteListNeedsRefresh: false,
       formData: {},
     };
   },
   getters: {
     goals: (state) => state.goals,
+    tasks: (state) => state.tasks,
+    notes: (state) => state.notes,
   },
   mutations: {
     setGoals(state, goals) {
       state.goals = goals;
-    },
-    setObjectives(state, objectives) {
-      state.objectives = objectives;
     },
     setTasks(state, tasks) {
       state.tasks = tasks;
@@ -36,121 +29,15 @@ export default createStore({
       state.notes = notes;
     },
 
-    pushCommand(state, command) {
-      state.commandStack.push(command);
-    },
-    popCommand(state) {
-      state.commandStack.pop();
-    },
-
-    setReturnValue(state, value) {
-      state.returnValue = value;
-    },
-    clearReturnValue(state) {
-      state.returnValue = { type: "null", data: null };
-    },
-
-    setCurrentRightPanelComponent(state, component) {
-      state.currentRightPanelComponent = component;
-    },
-    setEditingGoal(state, goal) {
-      state.editingGoal = goal;
-    },
 
     setGoalListNeedsRefresh(state, value) {
       state.goalListNeedsRefresh = value;
-    },
-    setObjectiveListNeedsRefresh(state, value) {
-      state.objectiveListNeedsRefresh = value;
     },
     setTaskListNeedsRefresh(state, value) {
       state.taskListNeedsRefresh = value;
     },
     setNoteListNeedsRefresh(state, value) {
       state.noteListNeedsRefresh = value;
-    },
-
-    updateEditorGoal(state, goalData) {
-      state.currentEditorGoal = goalData;
-    },
-    updateEditorObjective(state, objectiveData) {
-      state.currentEditorObjective = objectiveData;
-    },
-    updateEditorTask(state, taskData) {
-      state.currentEditorTask = taskData;
-    },
-
-    resetEditorGoal(state) {
-      state.currentEditorGoal = {
-        id: createUID(),
-        title: "",
-        details: "",
-        timeFrame: "",
-        objectives: [],
-        modified: false,
-        new: true,
-        saved: false,
-      };
-    },
-    resetEditorObjective(state) {
-      state.currentEditorObjective = {
-        id: createUID(),
-        title: "",
-        details: "",
-        timeFrame: "",
-        tasks: [],
-        modified: false,
-        new: true,
-        saved: false,
-      };
-    },
-    resetEditorTask(state) {
-      state.currentEditorTask = {
-        id: createUID(),
-        title: "",
-        details: "",
-        timeFrame: "",
-        modified: false,
-        new: true,
-        saved: false,
-      };
-    },
-
-    markEditorGoalModified(state) {
-      if (state.currentEditorGoal) state.currentEditorGoal.modified = true;
-    },
-    markEditorObjectiveModified(state) {
-      if (state.currentEditorObjective)
-        state.currentEditorObjective.modified = true;
-    },
-    markEditorTaskModified(state) {
-      if (state.currentEditorTask) state.currentEditorTask.modified = true;
-    },
-
-    addEditorObjectiveToEditorGoal(state) {
-      state.currentEditorGoal.objectives.push({
-        phase: 1,
-        data: state.currentEditorObjective,
-      });
-      state.currentEditorObjective = {
-        title: "",
-        details: "",
-        timeFrame: "",
-        tasks: [],
-        requiresWrite: true,
-      };
-    },
-    addEditorTaskToEditorObjective(state) {
-      state.currentEditorObjective.tasks.push({
-        phase: 1,
-        data: state.currentEditorTask,
-      });
-      state.currentEditorTask = {
-        title: "",
-        details: "",
-        timeFrame: "",
-        requiresWrite: true,
-      };
     },
 
     setFormDataField(state, data) {
@@ -164,74 +51,32 @@ export default createStore({
     },
   },
   actions: {
-    async fetchTopLevelGoals({ commit }) {
+    async fetchGoals({ commit }) {
       const response = await dataService.listGoals();
       commit("setGoals", response.data);
       commit("setGoalListNeedsRefresh", false);
-    },
-    
-    async fetchNotes({ commit }) {
-      const response = await dataService.listNotes();
-      commit("setNotes", response.data);
-      commit("setNoteListNeedsRefresh", false);
-    },
-    async fetchObjectives({ commit }) {
-      const response = await dataService.listObjectives();
-      commit("setObjectives", response.data);
-      commit("setObjectiveListNeedsRefresh", false);
     },
     async fetchTasks({ commit }) {
       const response = await dataService.listTasks();
       commit("setTasks", response.data);
       commit("setTaskListNeedsRefresh", false);
     },
+    async fetchNotes({ commit }) {
+      const response = await dataService.listNotes();
+      commit("setNotes", response.data);
+      commit("setNoteListNeedsRefresh", false);
+    },
     async createGoal({ commit }, goalData) {
       await dataService.createGoal(goalData);
       commit("setGoalListNeedsRefresh", true);
     },
+    async createTask({ commit }, taskData) {
+      await dataService.createTask(taskData);
+      commit("setTaskListNeedsRefresh", true);
+    },
     async createNote({ commit }, noteData) {
       await dataService.createNote(noteData);
       commit("setNoteListNeedsRefresh", true);
-    },
-    saveGoal({ commit, state }, goalData) {
-      const action = state.editingGoal ? "updateGoal" : "createGoal";
-      return dataService[action](goalData).then(() => {
-        commit("setEditingGoal", null);
-        commit("setGoalListNeedsRefresh", true);
-      });
-    },
-    async deleteGoal({ dispatch }, goalId) {
-      try {
-        await dataService.deleteGoal(goalId);
-        dispatch("fetchTopLevelGoals");
-      } catch (error) {
-        console.error("Error deleting goal:", error);
-      }
-    },
-    editGoal({ commit }, goal) {
-      commit("setEditingGoal", goal);
-      commit("setCurrentRightPanelComponent", "EditGoal");
-    },
-
-    async deleteObjective({ dispatch }, objectiveId) {
-      try {
-        await dataService.deleteObjective(objectiveId);
-        dispatch("fetchObjectives");
-      } catch (error) {
-        console.error("Error deleting objective:", error);
-      }
-    },
-    async deleteTask({ dispatch }, taskId) {
-      try {
-        await dataService.deleteTask(taskId);
-        dispatch("fetchTask");
-      } catch (error) {
-        console.error("Error deleting objective:", error);
-      }
-    },
-
-    changeRightPanelComponent({ commit }, component) {
-      commit("setCurrentRightPanelComponent", component);
     },
   },
 });
